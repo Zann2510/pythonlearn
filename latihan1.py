84ec88959c9a4a56a5c364a07b73acf5bf63c97b
Zann2510 <@zanndevictoria@gmail.com>
2025-09-11 12:02:15 +0700

    first commit

---
+++ latihan1.py
@@ -0,0 +1,12 @@
+a = 2 + 4
+print ("hasil penjumlahan" , a)
+
+angka = 3
+angka2 = 5 + angka
+nama = ['Ade', 'Aira','Bagus','Boby']
+nama.append('Beny')
+print (nama)
+print (len(nama))
+
+if angka2 > 7:
+    print (f"angka {angka2} lebih besar dari 7")
